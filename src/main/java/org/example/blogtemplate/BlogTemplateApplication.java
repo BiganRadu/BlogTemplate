2fe0cfa66b60106062f55ae065b07ec839c27106
Pissoi <raduxstream@gmail.com>
2024-01-03 14:47:15 +0200

    first commit

---
+++ src/main/java/org/example/blogtemplate/BlogTemplateApplication.java
@@ -0,0 +1,13 @@
+package org.example.blogtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/blogtemplate/BlogTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.blogtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
